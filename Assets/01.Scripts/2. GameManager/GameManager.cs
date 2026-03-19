c542c4a2756e1eea95024711d15a0fc30e4f3d44
thlee99813@gmail.com <thlee99813@gmail.com>
2026-03-19 11:08:32 +0900

    [이태환] 게임매니저 작업중

--- Assets/01.Scripts/2. GameManager/GameManager.cs
+++ Assets/01.Scripts/2. GameManager/GameManager.cs
@@ -1,14 +1,43 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    void Start()
+
+    public override void Init() { }
+
+    [Header("Game Flow")]
+    [SerializeField] private bool startFromOpeningCutscene = true;
+    [SerializeField] private float trainArrivalTime = 90f;
+
+    public GameState CurrentState { get; private set; } = GameState.None;
+
+
+    private void OnEnable()
     {
-        
+        EventManager.Instance.AddListener(MEventType.PlayerDied, OnPlayerDied);
+        EventManager.Instance.AddListener(MEventType.StageCleared, OnStageCleared);
     }
 
-    void Update()
+    private void OnDisable()
     {
-        
+        if (EventManager.Instance == null)
+            return;
+            
+        EventManager.Instance.RemoveListener(MEventType.PlayerDied, this);
+        EventManager.Instance.RemoveListener(MEventType.StageCleared, this);
     }
+
+    private void OnPlayerDied(MEventType type, Component sender, System.EventArgs args)
+    {
+        Debug.Log("GameOver");
+        ChangeState(GameState.GameOver);
+
+    }
+
+    private void OnStageCleared(MEventType type, Component sender, System.EventArgs args)
+    {
+        Debug.Log("GameClear");
+    }
+
+    
 }
